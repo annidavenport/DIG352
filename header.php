4ec4bec1a6b5e3e5d935c7994c6f6ea6c4dd892c
Anni Davenport <hello@littletinrocket.com>
2015-07-22 21:43:32 +1000

    Initialisation

---
+++ header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <!-- Basic Page Needs
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta charset="utf-8">
+  <title>Little Tin Rocket</title>
+  <meta name="description" content="Little Tin Rocket - Freelance Web Development">
+  <meta name="author" content="Anni Davenport">
+
+  <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- FONT
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700|Crimson+Text:400,600,400italic' rel='stylesheet' type='text/css'>
+
+  <!-- CSS
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/skeleton.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/type.css">
+  <link rel="stylesheet" href="css/responsive.css">
+
+  <!-- Favicon
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="icon" type="image/png" href="images/favicon.png">
+
+</head>
+<body>
+
+  <!-- Primary Page Layout
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <div class="">
---
+++ index.php
@@ -0,0 +1,55 @@
+<?php include('header.php') ?>
+
+    <!-- Hero Unit Document
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <section id="hero-unit" class="row">
+      <div class="container">
+        <h1>Logo goes here</h1>
+      </div>
+
+    </section>
+
+    <!-- About Section
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <section id="about" class="row">
+        <div class="container">
+
+            <h1>About</h1>
+        </div>
+    </section>
+
+    <!-- Portfolio Section
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <section id="portfolio" class="row">
+        <div class="container">
+            <h1>Portfolio</h1>
+        </div>
+    </section>
+
+    <!-- Services Section
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <section id="services" class="row">
+        <div class="container">
+            <h1>Services</h1>
+        </div>
+    </section>
+
+    <!-- Blog Section
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <section id="blog" class="row">
+        <div class="container">
+            <h1>Blog</h1>
+        </div>
+    </section>
+
+    <!-- Contact Section
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <section id="contact" class="row">
+        <div class="container">
+            <h1>Contact</h1>
+        </div>
+    </section>
+
+<?php include('footer.php') ?>
+
+
